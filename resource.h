4c06cde8713978ed16491f8f24311b9dba7a604b
TonyHarte <Tony.Harte1@gmail.com>
2017-05-30 09:25:20 +0100

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MP3TagSet    .rc
+//
+#define IDI_ICON1                       104
+#define IDD_DIALOG1                     106
+#define IDC_LIST                        501
+#define IDC_CURR_GROUPBOX               502
+#define IDC_PROPOSED_GROUPBOX           503
+
+
+#define IDS_FIRSTCOLUMN                 1001
+#define IDS_STRING1002                  1002
+#define IDS_STRING1003                  1003
+#define IDS_STRING1004                  1004
+#define IDS_STRING1005                  1005
+#define IDS_STRING1006                  1006
+
+#define MP3TagSet_MENU                  4000
+#define IDM_FILE_SELECTDIRECTORY        4001
+#define IDM_FILE_EXIT                   4002
+#define IDM_ABOUT                       4003
